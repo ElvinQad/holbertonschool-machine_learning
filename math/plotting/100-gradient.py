e06d5e3cb8c05bbe7ee909a37b22208ce9d90e0b
ElvinQad <qadirov.elvin.qe@gmail.com>
2025-04-04 19:46:03 +0400

    Night Shift

--- math/plotting/100-gradient.py
+++ math/plotting/100-gradient.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
+""" plot mountain elevation as a scatter plot with colorbar """
 import numpy as np
 import matplotlib.pyplot as plt
 
-np.random.seed(5)
+def gradient():
 
-x = np.random.randn(2000) * 10
-y = np.random.randn(2000) * 10
-z = np.random.rand(2000) + 40 - np.sqrt(np.square(x) + np.square(y))
+    np.random.seed(5)
 
-sc = plt.scatter(x, y, c=z)
-plt.colorbar(sc, label="elevation (m)")
-plt.title("Mountain Elevation")
-plt.xlabel('x coordinate (m)')
-plt.ylabel('y coordinate (m)')
-plt.show()
+    x = np.random.randn(2000) * 10
+    y = np.random.randn(2000) * 10
+    z = np.random.rand(2000) + 40 - np.sqrt(np.square(x) + np.square(y))
+    plt.figure(figsize=(6.4, 4.8))
+
+    plt.xlabel("x coordinate (m)")
+    plt.ylabel("y coordinate (m)")
+    plt.title("Mountain Elevation")
+
+    scatter = plt.scatter(x, y, c=z, cmap='viridis')
+    cbar = plt.colorbar(scatter)
+    cbar.set_label('elevation (m)')
+
+    plt.show()
