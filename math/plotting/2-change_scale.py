b28f56f78779d3f62873aa20cf0ec1c87fa31c93
ElvinQad <qadirov.elvin.qe@gmail.com>
2025-04-04 18:34:19 +0400

    Night Shift

--- math/plotting/2-change_scale.py
+++ math/plotting/2-change_scale.py
@@ -4,19 +4,18 @@ import matplotlib.pyplot as plt
 
 def change_scale():
     """
-    Plots exponential decay of C-14 with logarithmic y-axis scale
+    Plots exponential decay of C-14 with logarithmic y-scale
     """
     x = np.arange(0, 28651, 5730)
     r = np.log(0.5)
     t = 5730
     y = np.exp((r / t) * x)
-
-    plt.plot(x, y, 'b-')  # Add explicit blue line style
+    plt.figure(figsize=(6.4, 4.8))
+    
+    plt.plot(x, y)
     plt.xlabel('Time (years)')
     plt.ylabel('Fraction Remaining')
     plt.title('Exponential Decay of C-14')
     plt.yscale('log')
-    plt.axis([0, 28650, 0.001, 1])
-    plt.grid(True, which='both', linestyle='--', alpha=0.7)
-    plt.tight_layout()
+    plt.xlim(0, 28650)
     plt.show()
