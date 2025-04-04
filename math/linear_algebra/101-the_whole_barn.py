0b8488e5b5e87465d2493aa4a5327f68735a415a
ElvinQad <qadirov.elvin.qe@gmail.com>
2025-04-04 17:59:43 +0400

    3

--- math/linear_algebra/101-the_whole_barn.py
+++ math/linear_algebra/101-the_whole_barn.py
@@ -8,12 +8,14 @@ def add_matrices(mat1, mat2):
         return None
     if len(mat1) != len(mat2):
         return None
+
     new_matrix = []
+    if type(mat1[0]) is not list:
+        return [x + y for x, y in zip(mat1, mat2)]
+
     for i in range(len(mat1)):
         element = add_matrices(mat1[i], mat2[i])
         if element is None:
             return None
         new_matrix.append(element)
-    if type(new_matrix[0]) is not list:
-        return list(x + y for x, y in zip(mat1, mat2))
     return new_matrix
