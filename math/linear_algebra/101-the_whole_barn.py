e523f6d1455703a0cccf09c1faf0c252d248558b
ElvinQad <qadirov.elvin.qe@gmail.com>
2025-04-04 17:42:41 +0400

    2

--- math/linear_algebra/101-the_whole_barn.py
+++ math/linear_algebra/101-the_whole_barn.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
 """Defines `add_matrices`."""
-import numpy as np
 
 
 def add_matrices(mat1, mat2):
     """Adds two matrices."""
-    mat1, mat2 = np.array(mat1), np.array(mat2)
-    if not mat1.shape == mat2.shape:
+    if (type(mat1) is not list or type(mat2) is not list):
         return None
-    return mat1 + mat2
+    if len(mat1) != len(mat2):
+        return None
+    new_matrix = []
+    for i in range(len(mat1)):
+        element = add_matrices(mat1[i], mat2[i])
+        if element is None:
+            return None
+        new_matrix.append(element)
+    if type(new_matrix[0]) is not list:
+        return [x + y for x, y in zip(mat1, mat2)]
+    return new_matrix
