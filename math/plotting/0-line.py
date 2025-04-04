695e2d141e249f25ae7fd94a7ea2fa5149b3f5f7
ElvinQad <qadirov.elvin.qe@gmail.com>
2025-04-04 18:27:00 +0400

    Night Shift

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -2,8 +2,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-y = np.arange(0, 11) ** 3
+def line():
 
-plt.plot(y, 'r-')
-plt.axis([0, 10, None, None])
-plt.show()
+    y = np.arange(0, 11) ** 3
+    plt.figure(figsize=(6.4, 4.8))
+
+    plt.plot(y, 'r-')
+    plt.axis([0, 10, 0, 1000])
+    plt.show()
--- math/plotting/2-change_scale.py
+++ math/plotting/2-change_scale.py
@@ -2,15 +2,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.arange(0, 28651, 5730)
-r = np.log(0.5)
-t = 5730
-y = np.exp((r / t) * x)
+def change_scale():
+    x = np.arange(0, 28651, 5730)
+    r = np.log(0.5)
+    t = 5730
+    y = np.exp((r / t) * x)
+    plt.figure(figsize=(6.4, 4.8))
 
-plt.plot(x, y)
-plt.xlabel('Time (years)')
-plt.ylabel('Fraction Remaining')
-plt.title('Exponential Decay of C-14')
-plt.yscale('log')
-plt.axis([0, 28650, None, None])
-plt.show()
+    plt.plot(x, y)
+    plt.xlabel('Time (years)')
+    plt.ylabel('Fraction Remaining')
+    plt.title('Exponential Decay of C-14')
+    plt.yscale('log')
+    plt.axis([0, 28650, 0.001, 1])
+    plt.show()
