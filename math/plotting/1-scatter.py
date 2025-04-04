d97d2a63e3ae7d5cf06d23fde1fcea8ed5d5c637
ElvinQad <qadirov.elvin.qe@gmail.com>
2025-04-04 18:31:49 +0400

    Night Shift

--- math/plotting/1-scatter.py
+++ math/plotting/1-scatter.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
+"""
+Module that creates a scatter plot showing the relationship
+between men's height and weight
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 
 def scatter():
+    """
+    Function that creates a scatter plot of men's height vs weight
+    """
     mean = [69, 0]
     cov = [[15, 8], [8, 15]]
     np.random.seed(5)
@@ -10,7 +17,7 @@ def scatter():
     y += 180
     plt.figure(figsize=(6.4, 4.8))
 
-    plt.plot(x, y, 'm.')
+    plt.scatter(x, y, color='magenta')
     plt.ylabel('Weight (lbs)')
     plt.xlabel('Height (in)')
     plt.title("Men's Height vs Weight")
--- math/plotting/2-change_scale.py
+++ math/plotting/2-change_scale.py
@@ -3,11 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def change_scale():
+    """
+    Plots exponential decay of C-14 with logarithmic y-axis scale
+    """
     x = np.arange(0, 28651, 5730)
     r = np.log(0.5)
     t = 5730
     y = np.exp((r / t) * x)
-    plt.figure(figsize=(6.4, 4.8))
 
     plt.plot(x, y)
     plt.xlabel('Time (years)')
