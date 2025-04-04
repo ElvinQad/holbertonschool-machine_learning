2dc95d586bc4a68cd056f44c02dddc084b7505ae
ElvinQad <qadirov.elvin.qe@gmail.com>
2025-04-04 19:23:27 +0400

    Night Shift

--- math/plotting/1-scatter.py
+++ math/plotting/1-scatter.py
@@ -1,24 +1,16 @@
 #!/usr/bin/env python3
-"""
-Module that creates a scatter plot showing the relationship
-between men's height and weight
-"""
+""" plots x, y as a scatter plot """
 import numpy as np
 import matplotlib.pyplot as plt
 
-def scatter():
-    """
-    Function that creates a scatter plot of men's height vs weight
-    """
-    mean = [69, 0]
-    cov = [[15, 8], [8, 15]]
-    np.random.seed(5)
-    x, y = np.random.multivariate_normal(mean, cov, 2000).T
-    y += 180
-    plt.figure(figsize=(6.4, 4.8))
+mean = [69, 0]
+cov = [[15, 8], [8, 15]]
+np.random.seed(5)
+x, y = np.random.multivariate_normal(mean, cov, 2000).T
+y += 180
 
-    plt.scatter(x, y, color='magenta')
-    plt.ylabel('Weight (lbs)')
-    plt.xlabel('Height (in)')
-    plt.title("Men's Height vs Weight")
-    plt.show()
+plt.scatter(x, y, c='m')
+plt.xlabel('Height (in)')
+plt.ylabel('Weight (lbs)')
+plt.title("Men's Height vs Weight")
+plt.show()
