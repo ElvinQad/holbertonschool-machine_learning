19eb0ed4abb2ae327e81041e86f8b90476430a1c
ElvinQad <qadirov.elvin.qe@gmail.com>
2025-04-04 18:41:37 +0400

    Night Shift

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -10,9 +10,10 @@ def frequency():
     """
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
-    plt.figure(figsize=(6.4, 4.8))
     
-    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
+    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black', linewidth=1)
+    plt.xlim(0, 100)
+    plt.ylim(0, 16)
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
     plt.title('Project A')
