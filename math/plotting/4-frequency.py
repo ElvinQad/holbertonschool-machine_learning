94805e488d1c55dd870f5b7214319b548ed14dc3
ElvinQad <qadirov.elvin.qe@gmail.com>
2025-04-04 19:13:59 +0400

    Night Shift

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -2,14 +2,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def frequency():
-    np.random.seed(5)
-    student_grades = np.random.normal(68, 15, 50)
-    plt.figure(figsize=(6.4, 4.8))
-    plt.hist(student_grades, bins=np.arange(0, 101, 10), 
-             edgecolor='black', linewidth=1, range=(0, 100))
-    plt.xlabel('Grades')
-    plt.ylabel('Number of Students')
-    plt.title('Project A')
-    plt.savefig('project_a.png')
-    plt.close()
+np.random.seed(5)
+student_grades = np.random.normal(68, 15, 50)
+
+plt.hist(student_grades,
+         bins=[0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
+         edgecolor='black')
+plt.xlabel('Grades')
+plt.ylabel('Number of Students')
+plt.title("Project A")
+plt.xlim((0, 100))
+plt.xticks(np.arange(0, 101, 10))
+plt.ylim((0, 30))
+plt.show()
